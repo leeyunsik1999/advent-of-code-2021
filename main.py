5ba2cf7e121a0c15ccdfec6ebcc1875b61d4437a
Yunsik Edwin Lee <leeyunsik1999@gmail.com>
2021-12-18 17:19:58 -0500

    Finished day 1

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+
+
+if __name__ == "__main__":
+    text = open("input.txt", "r")
+    line = text.readline()
+    prevnum = None
+    count = 0
+    while line:
+        line = line.strip("\n")
+        if prevnum == None:
+            prevnum = int(line)
+        else:
+            num = int(line)
+            count += 1 if prevnum < num else 0
+            prevnum = num
+        line = text.readline()
+    print(f"Total increases: %d" % (count))
+    text.close()
